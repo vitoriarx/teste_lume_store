3c3aa04db95a98b7d05269528891ac7eb3404d18
vitoriarx <vitoriasantos78690@gmail.com>
2026-02-20 23:13:41 -0300

    configuração do ambiente

---
+++ playwright.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+
+  timeout: 30 * 1000,
+
+  use: {
+    baseURL: 'https://magento2-demo.magebit.com/',
+
+    headless: true,
+
+    trace: 'on-first-retry',
+
+    screenshot: 'only-on-failure',
+
+    video: 'retain-on-failure',
+  },
+
+  reporter: [
+    ['html', { open: 'never' }]
+  ],
+});
